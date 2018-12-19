73ee636c2e750e1d48f55afa0dee14da196b5e8b
Mark Clifton <mark.clifton@qrctech.com>
2018-12-19 18:19:58 -0500

    Fix Dependencies

---
+++ dependencies/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mark/Desktop/Projects/GameProject2018/libs/glm/glm-0.9.9.2/glm
+# Build directory: /home/mark/Desktop/Projects/GameProject2018/libs/glm/glm-0.9.9.2/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
