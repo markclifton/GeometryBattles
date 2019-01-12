d66c21c8d21f32ce86ea5947caa5f240b6ee71e9
Mark Clifton <mark.clifton@qrctech.com>
2019-01-12 12:19:34 -0500

    Fix Audio and start Game code

--- game/levels/sandbox.cpp
+++ game/levels/sandbox.cpp
@@ -5,7 +5,12 @@
 
 #include "ps/managers/shadermanager.h"
 #include "ps/drawable/triangle.h"
+#include "ps/drawable/rectangle.h"
 #include "ps/ecs/ecsmanager.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/transform.hpp>
+#include "ps/managers/soundmanager.h"
 
 std::string Sandbox::CONTEXT_NAME = "Sandbox";
 
@@ -15,9 +20,6 @@ Sandbox::Sandbox()
 
 void Sandbox::run()
 {
-    std::cout << "Running Sandbox!" << std::endl;
-    sleep(1);
-
     auto s = ps::managers::ShaderManager::Get().getShader("Base");
     s->bind();
     s->setUniform("projection", glm::mat4(1.));
@@ -32,5 +34,13 @@ void Sandbox::loadResources()
     ps::managers::ShaderManager::Get().loadShader("Base", "resources/shaders/basic.vs", "resources/shaders/basic.fs");
 
     auto tri = std::make_shared<ps::drawable::Triangle>(CONTEXT_NAME, glm::vec3(0,0,-1), ps::managers::ShaderManager::Get().getShader("Base"));
-    ps::ecs::ECSManager::get().addEntity(tri);
+    //ps::ecs::ECSManager::get().addEntity(tri);
+
+    auto rect = std::make_shared<ps::drawable::Rectangle>(CONTEXT_NAME, glm::vec3(0,0,-1), ps::managers::ShaderManager::Get().getShader("Base"));
+    rect->setColor(glm::vec4(1,1,1,1));
+    rect->setTransform(glm::scale(glm::vec3(.5,.5,.5)));
+    ps::ecs::ECSManager::get().addEntity(rect);
+
+    ps::managers::SoundManager::Get().loadSound("Rain", "resources/sounds/rain.mp3");
+    ps::managers::SoundManager::Get().playSound("Rain");
 }
